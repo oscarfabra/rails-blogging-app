a7138688cf2dd26b552a8a6a81fded20d7cc57d0
Oscar Fabra <oscarfabra@gmail.com>
2014-12-23 22:46:56 -0500

    Added confirmable option to users. Sending confirmation emails correctly.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,20 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Configures mailer to use Gmail over SMTP.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      user_name: Rails.application.secrets.smtp_user_name,
+      password: Rails.application.secrets.smtp_password,
+      authentication: 'plain',
+      enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
